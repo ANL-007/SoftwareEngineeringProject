482459b2debd67d0850c7a60a657343a4dc49e4f
ANL-007 <anicolaulourenco@ufl.edu>
2025-10-04 21:57:34 -0400

    Change settings.py file

--- backend/studyhub/settings.py
+++ backend/studyhub/settings.py
@@ -25,9 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY', 'fallback-secret-key')
-DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
